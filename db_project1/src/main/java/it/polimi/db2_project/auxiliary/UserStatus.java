e1bb9d95026d8fc319bfdf8412b06095e14f423b
giusti_leo <leonardo.giusti@mail.polimi.it>
2021-05-03 09:47:40 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	db_project1/src/main/java/it/polimi/db2_project/servlets/Login.java

---
+++ db_project1/src/main/java/it/polimi/db2_project/auxiliary/UserStatus.java
@@ -0,0 +1,8 @@
+package it.polimi.db2_project.auxiliary;
+
+import java.io.Serializable;
+
+
+public enum UserStatus implements Serializable {
+    BANNED, COMPLETED, NOT_COMPLETED, NOT_AVAILABLE;
+}
